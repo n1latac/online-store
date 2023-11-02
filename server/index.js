e263951fdc8fdbb33f27c3c07b1cbec218bc9c97
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-11-02 19:17:41 +0200

    add dir

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`)
-})
+const start = async()=>{
+    try {
+        app.listen(PORT, ()=>{
+            console.log(`Server started on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
