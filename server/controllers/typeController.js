70dc1c7f959b6436ee72d081fc9892e6ede11b48
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-11-03 02:16:34 +0200

    insert type to db

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,6 +1,9 @@
+const {Type} = require('../models/model')
 class TypeController {
     async create(req, res){
-
+        const {name} = req.body
+        const type = await Type.create({name: name})
+        return res.json(type)
     }
     async getAll(req, res){
 
